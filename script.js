fbafd6fa3a183570657ee935884d582810dc8743
Inskipp <71417979+HaroldPetersInskipp@users.noreply.github.com>
2025-09-01 08:18:15 -0600

    Update script.js
    Add update URL

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Log URLs
 // @namespace    https://github.com/HaroldPetersInskipp/
-// @version      0.4.1
+// @version      0.4.2
 // @homepageURL  https://github.com/HaroldPetersInskipp/http-post-urls
 // @supportURL   https://github.com/HaroldPetersInskipp/http-post-urls/issues
+// @downloadURL  https://gist.github.com/HaroldPetersInskipp/7476f0d7c9512b80b90211c0b5b90663/raw/http-post-urls.user.js
+// @updateURL    https://gist.github.com/HaroldPetersInskipp/7476f0d7c9512b80b90211c0b5b90663/raw/http-post-urls.user.js
 // @description  POST all URLs visited to a local server, excluding local files
 // @author       Inskipp
 // @copyright    2024+, HaroldPetersInskipp (https://github.com/HaroldPetersInskipp)
